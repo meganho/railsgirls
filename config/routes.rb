be379b6e6e5db4af3d700e64d7cdd98d57ac3709
megan <bbeeaanns@gmail.com>
2017-02-11 15:08:32 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :comments
+  get 'pages/info'
+
+  get "pages/info"
+  root to: redirect('/ideas')
+  root to: "ideas#index"
+  resources :ideas
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
